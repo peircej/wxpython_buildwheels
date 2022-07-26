7ee49f235d32a16fbcdb6bb36af1962d0128b80f
Jon Peirce <jon.peirce@gmail.com>
2022-07-26 16:06:58 +0100

    yum installs now all work, adding python packages

--- install_depends.sh
+++ install_depends.sh
@@ -2,3 +2,5 @@
 dnf config-manager --set-enabled powertools  # needed for SDL2-devel
 yum install -y freeglut-devel mesa-libGL-devel mesa-libGLU-devel gstreamer1-plugins-base-devel
 yum install -y gtk3-devel libnotify-devel libjpeg-devel libtiff-devel libpng-devel SDL2-devel libSM-devel webkitgtk3-devel libXtst-devel
+
+pip install attrdict
