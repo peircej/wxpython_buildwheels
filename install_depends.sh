d380c0711d6ba5bca809a8e88a1cbf0c95524512
Jon Peirce <jon.peirce@gmail.com>
2022-07-26 15:19:53 +0100

    dnf set-enabled not yum set-enabled

--- install_depends.sh
+++ install_depends.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-yum config-manager --set-enabled powertools  # needed for SDL2-devel
+dnf config-manager --set-enabled powertools  # needed for SDL2-devel
 yum install -y freeglut-devel mesa-libGL-devel mesa-libGLU-devel gstreamer1-plugins-base-devel
 yum install -y gtk3-devel libnotify-devel libjpeg-devel libtiff-devel libpng-devel SDL2-devel libSM-devel webkit2gtk3-devel libXtst-devel
