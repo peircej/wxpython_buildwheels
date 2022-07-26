98028b1d0372baecd0c98c18cd901f37c1996b3b
Jon Peirce <jon.peirce@gmail.com>
2022-07-26 14:06:04 +0100

    Use manylinux2014 not 2010

--- install_depends.sh
+++ install_depends.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-yum install -y dpkg-dev build-essential ubuntu-restricted-extras
+yum install -y dpkg-dev
 yum install -y freeglut-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev
 yum install -y libgtk-3-dev libnotify-dev libjpeg-dev libtiff-dev libpng-dev libsdl2-dev libsm-dev libwebkit2gtk-4.0-dev libxtst-dev
