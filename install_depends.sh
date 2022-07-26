d187c289be12af0399431cab17273f77065625fa
Jon Peirce <jon.peirce@gmail.com>
2022-07-26 15:02:08 +0100

    yum has different names to ubuntu

--- install_depends.sh
+++ install_depends.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-yum install -y dpkg-dev
-yum install -y freeglut-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev
-yum install -y libgtk-3-dev libnotify-dev libjpeg-dev libtiff-dev libpng-dev libsdl2-dev libsm-dev libwebkit2gtk-4.0-dev libxtst-dev
+sudo yum config-manager --set-enabled powertools  # needed for SDL2-devel
+yum install -y freeglut-devel mesa-libGL-devel mesa-libGLU-devel gstreamer1-plugins-base-devel
+yum install -y gtk3-devel libnotify-devel libjpeg-devel libtiff-devel libpng-devel SDL2-devel libSM-devel libwebkit2gtk3-devel libXtst-devel
