8593f3a3e8c846c3ab64e409475b8d0f44b5ff92
Jon Peirce <jon.peirce@gmail.com>
2022-07-26 12:59:45 +0100

    Try moving depenency install into cibuildwheel config

---
+++ install_depends.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt install -y dpkg-dev build-essential ubuntu-restricted-extras
+sudo apt install -y freeglut3-dev libgl1-mesa-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev
+sudo apt install -y libgtk-3-dev libnotify-dev libjpeg-dev libtiff-dev libpng-dev libsdl2-dev libsm-dev libwebkit2gtk-4.0-dev libxtst-dev
